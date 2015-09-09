d75323e131a5a981f3ad9cd6916122aa3dfbb836
Daniel Treasure <dbtreasure@gmail.com>
2015-09-09 15:18:44 -0600

    adds avatar images and cleans up code a touch

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 var app = angular.module("app", []);
 
 app.controller("main-ctrl", ["$scope", function($scope){
-	$scope.friends = [
+	$scope.characters = [
 		{
 			name: "Rick",
 			id: 1,
 			age: null,
+			avatar: "http://vignette4.wikia.nocookie.net/rickandmorty/images/d/dd/Rick.png/revision/latest?cb=20131230003659",
 			status: "alive",
-			bio: "Richard 'Rick' Sanchez is the co-eponymous main character and leading protagonist of the show. He is a genius scientist whose alcoholism and reckless, sociopathic behavior are a source of concern for his daughter's family over the safety of their son, Morty."
+			bio: "Richard 'Rick' Sanchez is the co-eponymous main character and leading protagonist of the show."
 		},
 		{
 			name: "Morty",
 			id: 2,
 			age: 14,
+			avatar: "http://vignette1.wikia.nocookie.net/rickandmorty/images/3/36/Morty_Interested_S1E11.JPG/revision/latest/scale-to-width-down/250?cb=20150908101833",
 			status: "alive",
-			bio: "Mortimer 'Morty' Smith Sr. is one of the two eponymous main protagonists in Rick and Morty. He is the grandson of Rick and is often forced to tag along on his various misadventures. Morty attends Harry Herpson High School along with his sister, Summer."
+			bio: "Mortimer 'Morty' Smith Sr. is one of the two eponymous main protagonists in Rick and Morty."
 		}
 	];
 
 	$scope.delete = function(id){
-		$scope.friends.forEach(function(friend, idx){
+		$scope.characters.forEach(function(friend, idx){
 			if(friend.id === id){
-				$scope.friends.splice(idx, 1)
+				$scope.characters.splice(idx, 1)
 			}
 		})
 	};
